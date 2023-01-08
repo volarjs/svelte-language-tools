1feeacd9a3ffb95993b7dfc2707b5f9275304fc7
johnsoncodehk <johnsoncodehk@gmail.com>
2023-01-08 15:11:49 +0800

    feat: add svelte packages

--- packages/language-core/src/index.ts
+++ packages/language-core/src/index.ts
@@ -1,95 +1,122 @@
-import { LanguageModule, VirtualFile, FileKind, FileCapabilities, FileRangeCapabilities } from "@volar/language-core";
-import * as html from 'vscode-html-languageservice';
+import { decode } from '@jridgewell/sourcemap-codec';
+import { VirtualFile, FileKind, LanguageModule, FileCapabilities, FileRangeCapabilities } from '@volar/language-core';
+import { svelte2tsx } from 'svelte2tsx';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+import { URI } from 'vscode-uri';
 
-export const languageModule: LanguageModule<Html1File> = {
+export * from '@volar/language-core';
+
+export const languageModule: LanguageModule = {
 	createFile(fileName, snapshot) {
-		if (fileName.endsWith('.html1')) {
-			return new Html1File(fileName, snapshot);
+		if (fileName.endsWith('.svelte')) {
+			return {
+				fileName,
+				snapshot,
+				kind: FileKind.TextFile,
+				embeddedFiles: getEmbeddedFiles(fileName, snapshot.getText(0, snapshot.getLength())),
+				capabilities: FileCapabilities.full,
+				mappings: [],
+			};
 		}
 	},
-	updateFile(html1File, snapshot) {
-		html1File.update(snapshot);
+	updateFile(sourceFile, snapshot) {
+		sourceFile.snapshot = snapshot;
+		sourceFile.embeddedFiles = getEmbeddedFiles(sourceFile.fileName, sourceFile.snapshot.getText(0, sourceFile.snapshot.getLength()));
 	},
 };
 
-const fullFileCapabilities: FileCapabilities = {
-	diagnostic: true,
-	foldingRange: true,
-	documentFormatting: true,
-	documentSymbol: true,
-	codeAction: true,
-	inlayHint: true,
-};
-const fullFileRangeCapabilities: FileRangeCapabilities = {
-	hover: true,
-	references: true,
-	definition: true,
-	rename: true,
-	completion: true,
-	diagnostic: true,
-	semanticTokens: true,
-};
+function getEmbeddedFiles(fileName: string, text: string) {
 
-const htmlLs = html.getLanguageService();
-
-export class Html1File implements VirtualFile {
-
-	kind = FileKind.TextFile;
-	capabilities = fullFileCapabilities;
-
-	fileName!: string;
-	mappings!: VirtualFile['mappings'];
-	embeddedFiles!: VirtualFile['embeddedFiles'];
-	document!: html.TextDocument;
-	htmlDocument!: html.HTMLDocument;
-
-	constructor(
-		public sourceFileName: string,
-		public snapshot: ts.IScriptSnapshot,
-	) {
-		this.fileName = sourceFileName + '.html';
-		this.onSnapshotUpdated();
-	}
-
-	public update(newSnapshot: ts.IScriptSnapshot) {
-		this.snapshot = newSnapshot;
-		this.onSnapshotUpdated();
-	}
-
-	onSnapshotUpdated() {
-		this.mappings = [{
-			sourceRange: [0, this.snapshot.getLength()],
-			generatedRange: [0, this.snapshot.getLength()],
-			data: fullFileRangeCapabilities,
-		}];
-		this.document = html.TextDocument.create(this.fileName, 'html', 0, this.snapshot.getText(0, this.snapshot.getLength()));
-		this.htmlDocument = htmlLs.parseHTMLDocument(this.document);
-		this.embeddedFiles = [];
-		this.addStyleTag();
-	}
-
-	addStyleTag() {
-		let i = 0;
-		this.htmlDocument.roots.forEach(root => {
-			if (root.tag === 'style' && root.startTagEnd !== undefined && root.endTagStart !== undefined) {
-				const styleText = this.snapshot.getText(root.startTagEnd, root.endTagStart);
-				this.embeddedFiles.push({
-					fileName: this.fileName + `.${i++}.css`,
-					kind: FileKind.TextFile,
-					snapshot: {
-						getText: (start, end) => styleText.substring(start, end),
-						getLength: () => styleText.length,
-						getChangeRange: () => undefined,
-					},
-					mappings: [{
-						sourceRange: [root.startTagEnd, root.endTagStart],
-						generatedRange: [0, styleText.length],
-						data: fullFileRangeCapabilities,
-					}],
-					capabilities: fullFileCapabilities,
-					embeddedFiles: [],
-				});
-			}
+	try {
+		const tsx = svelte2tsx(text, {
+			filename: fileName,
+			isTsFile: true,
+			mode: 'ts',
 		});
+		const v3Mappings = decode(tsx.map.mappings);
+		const sourcedDoc = TextDocument.create(URI.file(fileName).toString(), 'svelte', 0, text);
+		const genDoc = TextDocument.create(URI.file(fileName + '.tsx').toString(), 'typescriptreact', 0, tsx.code);
+		const mappings: VirtualFile['mappings'] = [];
+
+		let current: {
+			genOffset: number,
+			sourceOffset: number,
+		} | undefined;
+
+		for (let genLine = 0; genLine < v3Mappings.length; genLine++) {
+			for (const segment of v3Mappings[genLine]) {
+				const genCharacter = segment[0];
+				const genOffset = genDoc.offsetAt({ line: genLine, character: genCharacter });
+				if (current) {
+					let length = genOffset - current.genOffset;
+					const sourceText = text.substring(current.sourceOffset, current.sourceOffset + length);
+					const genText = tsx.code.substring(current.genOffset, current.genOffset + length);
+					if (sourceText !== genText) {
+						length = 0;
+						for (let i = 0; i < genOffset - current.genOffset; i++) {
+							if (sourceText[i] === genText[i]) {
+								length = i + 1;
+							}
+							else {
+								break;
+							}
+						}
+					}
+					if (length > 0) {
+						const lastMapping = mappings.length ? mappings[mappings.length - 1] : undefined;
+						if (lastMapping && lastMapping.generatedRange[1] === current.genOffset && lastMapping.sourceRange[1] === current.sourceOffset) {
+							lastMapping.generatedRange[1] = current.genOffset + length;
+							lastMapping.sourceRange[1] = current.sourceOffset + length;
+						}
+						else {
+							mappings.push({
+								sourceRange: [current.sourceOffset, current.sourceOffset + length],
+								generatedRange: [current.genOffset, current.genOffset + length],
+								data: FileRangeCapabilities.full,
+							});
+						}
+					}
+					current = undefined;
+				}
+				if (segment[2] !== undefined && segment[3] !== undefined) {
+					const sourceOffset = sourcedDoc.offsetAt({ line: segment[2], character: segment[3] });
+					current = {
+						genOffset,
+						sourceOffset,
+					};
+				}
+			}
+		}
+
+		const embeddedFiles: VirtualFile[] = [];
+
+		embeddedFiles.push({
+			fileName: fileName + '.ts',
+			snapshot: {
+				getText(start, end) {
+					return tsx.code.substring(start, end);
+				},
+				getLength() {
+					return tsx.code.length;
+				},
+				getChangeRange() {
+					return undefined;
+				},
+			},
+			kind: FileKind.TypeScriptHostFile,
+			capabilities: {
+				...FileCapabilities.full,
+				foldingRange: false,
+				documentSymbol: false,
+				documentFormatting: false,
+			},
+			mappings: mappings,
+			embeddedFiles: [],
+		});
+
+		return embeddedFiles;
+	}
+	catch {
+		return [];
 	}
 }
