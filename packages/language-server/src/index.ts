e928900b775780045fab8b7f9d4bd541a4540f1d
leidonglin <leidonglin@xiaomi.com>
2023-07-21 16:45:45 +0800

    feat: upgrade to 1.9.0 and update to volar-service-typescript

--- packages/language-server/src/index.ts
+++ packages/language-server/src/index.ts
@@ -1,30 +1,22 @@
-import * as createTsPlugin from '@volar-plugins/typescript';
-import { createConnection, startLanguageServer, LanguageServerPlugin } from '@volar/language-server/browser';
+import createTsPlugin from 'volar-service-typescript';
+import { createConnection, startLanguageServer, LanguageServerPlugin } from '@volar/language-server/node';
 import { languageModule } from '@volar-examples/svelte-language-core';
 
 const connection = createConnection();
 const baseExts = ['js', 'cjs', 'mjs', 'ts', 'cts', 'mts', 'jsx', 'tsx', 'json', 'svelte'];
 const plugin: LanguageServerPlugin = (): ReturnType<LanguageServerPlugin> => {
 	return {
-		tsconfigExtraFileExtensions: [{ extension: 'svelte', isMixedContent: true, scriptKind: 7 }],
-		diagnosticDocumentSelector: [
-			{ language: 'javascript' },
-			{ language: 'typescript' },
-			{ language: 'javascriptreact' },
-			{ language: 'typescriptreact' },
-			{ language: 'svelte' },
-		],
-		extensions: {
-			fileRenameOperationFilter: baseExts,
-			fileWatcher: baseExts,
-		},
+		extraFileExtensions: [{ extension: 'svelte', isMixedContent: true, scriptKind: 7 }],
+		watchFileExtensions: baseExts,
 		resolveConfig(config) {
 
-			config.plugins ??= {};
-			config.plugins.typescript ??= createTsPlugin();
+			config.services ??= {};
+			config.services.typescript ??= createTsPlugin();
 
 			config.languages ??= {};
 			config.languages.svelte ??= languageModule;
+
+			return config;
 		},
 	}
 };
